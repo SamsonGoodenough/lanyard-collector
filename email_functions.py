163eea49cbd1800c79995983a0aeacc6ddd2356c
Samson Goodenough <goodenough.samson@gmail.com>
2020-07-17 23:31:35 -0400

    Finished email body

--- email_functions.py
+++ email_functions.py
@@ -1,12 +1,23 @@
+import unicodedata
+
 def make_email(school):
   subject = f'Hey {school}, I was wondering something.'
   body = f'''Dear {school}, 
 
-I was wondering if you would help.  
+My name is Samson Goodenough, and yes, that's my real name.  I'm a student studying computer science and mathematics currently living in London Ontario, Canada. 
 
-Thanks, 
- - Samson Goodenough'''
+I have a bit of an odd hobby, I like to travel to many universities and colleges to attend their school fairs in order to collect a lanyard from them to add to my growing collection.
+Right now I have just over 40 lanyards in my collection, and this is where you come in! I was wondering if you would like to help me further expand my collection by sending me one of your school's lanyards.  
 
+As I said before, I live in Canada.  Although I've gone very out of my way to acquire more for my collection, somewhere that I've never gotten the chance to go hunting for them is just over the border.
+So now I ask you, would you please send me a {school} branded lanyard?
+
+I would be so gracious if you were to consider sparing one, please let me know if you think this is possible. 
+
+Sincerely, 
+ - Samson Goodenough'''
+
+  body = unicodedata.normalize("NFKD", body)
   return subject, body
 
 def send_email(subject, body, school, to, server, sent_from):
